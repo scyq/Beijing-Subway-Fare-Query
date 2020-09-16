31a6028c4007fec38aa5248e39c653b9c1094f5f
Yuqing_Chen <chenyuqing0429@foxmail.com>
2020-09-16 22:52:01 +0800

    更新了滑轮缩放和鼠标拖动功能

--- script/run.js
+++ script/run.js
@@ -4,13 +4,19 @@
 var mapData = undefined;
 var viewportMaxX = 2000;    // 视窗最大X
 var viewportMaxY = 2000;    // 视窗最大Y
+
+// 地铁站绘图信息
 var stationDrawer;  // 地铁站绘图
 var stationRadius;  // 地铁站半径
+var stationStroke;  // 地铁外围空心圈
+
+
 var lineDrawer;
 
 // captionOffset是地铁站的文字说明关于地铁站图标的偏移量
 var captionOffsetMaxX = 70;
 var captionOffsetMaxY = 50;
+var captionFontSize = undefined; // 地铁站名标注
 
 // 类定义
 /*
@@ -71,6 +77,49 @@ SVG.on(document, "DOMContentLoaded", function(){
     var borderRect = draw.rect(viewportMaxX, viewportMaxY - 2 * stationRadius).move(0, stationRadius * 2).stroke({color: "grey", width: 5}).fill("transparent");
     lineDrawer = draw.nested();
     stationDrawer = draw.nested();
+    // route 寻路没有写
+
+    // input是监听值变化，拖动条会改变zoom值，所以第一个参数是input
+    document.getElementById("zoomSize").addEventListener("input", function(){
+        let ratio = Math.exp((this.value - 40) / 20)
+        draw.size(100 * ratio + "%", 100 * ratio + "%")
+    });
+
+    // 一旦SVG初始化，初始化地图信息List
+    mapData = {
+        station: {},
+        lines: {}
+    };
+
+    // 处理地图拖动，利用svg.draggble
+    let frameDragStartX = undefined;
+    let frameDragStartY = undefined;
+    let frameScrollStartX = undefined;
+    let frameScrollStartY = undefined;
+
+    borderRect.draggable().on("dragstart", function(e){
+        frameDragStartX = e.detail.event.clientX;
+        frameDragStartY = e.detail.event.clientY;
+        frameScrollStartX = document.getElementById("displayPort").scrollLeft;
+        frameScrollStartY = document.getElementById("displayPort").scrollTop;
+    });
+
+    borderRect.draggable().on("dragmove", function(e){
+        e.preventDefault()
+        let offsetX = e.detail.event.clientX - frameDragStartX
+        let offsetY = e.detail.event.clientY - frameDragStartY
+        document.getElementById("displayPort").scrollLeft = frameScrollStartX  - offsetX
+        document.getElementById("displayPort").scrollTop = frameScrollStartY  - offsetY
+    })
+
+    // 处理鼠标滚轮缩放
+    document.getElementById("displayPort").addEventListener("mousewheel", function(e){
+        e.preventDefault()
+        var zoomSize = document.getElementById("zoomSize");
+        zoomSize.value -= e.deltaY / 10;
+        SVG.adopt(zoomSize).fire("input");
+    })
+
 
 });
 
@@ -94,4 +143,3 @@ $('#loadFromLocal').button();
 $('#importJson').button();
 $('#exportJson').button();
 
-// 2020.9.16 我没有时间写代码 sad :(
