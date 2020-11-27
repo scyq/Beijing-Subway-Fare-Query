757ceece37cf559cb2934687c9310abb58ca7d2c
Yuqing_Chen <chenyuqing0429@foxmail.com>
2020-11-27 11:49:01 +0800

    成功插入地铁图

--- src/Map.js
+++ src/Map.js
@@ -3,23 +3,35 @@
     地图展示类
  */
 
- import React from 'react';
+import React from 'react';
 
- class Map extends React.Component{
+class Map extends React.Component {
 
-    showMap() {
-        
+    createSubway (){
+        let BMapSub = window.BMapSub;
+        var subwayCityName = '北京';
+        var list = BMapSub.SubwayCitiesList;
+        var subwaycity = null;
+        for (var i = 0; i < list.length; i++) {
+            if (list[i].name === subwayCityName) {
+                subwaycity = list[i];
+                break;
+            }
+        }
+        var subway = new BMapSub.Subway('map', subwaycity.citycode);
+        subway.setZoom(1);
+    }
+
+    componentDidMount() {
+        this.createSubway();
     }
 
     render() {
         return (
             <div id="map">
-                
-                地图
-
             </div>
         );
     }
- }
+}
 
- export default Map;
+export default Map;
